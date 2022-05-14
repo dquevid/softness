3bda54537242394a9e400f1d9976b1c900847634
DQVID <david.lord.ru@gmail.com>
2022-05-14 08:55:18 +0300

    alpha 0.2

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-let referToTag = (selector) => {
+let currentDate = new Date()
+
+function referToTag(selector) {
 	return document.querySelector(selector)
 }
 
-let referToTags = (selector) => {
+function referToTags(selector) {
 	return document.querySelectorAll(selector)
 }
 
@@ -11,8 +13,14 @@ document.onmousemove = e => {
 	let biasY = window.innerHeight/2
 	let textShadowParameters = `${(e.clientX - biasX)/biasX*2.5}px ${(e.clientY - biasY)/biasY*2.5}px 7.5px #0000001f`
 	let shadowParameters = `${(e.clientX - biasX)/biasX*2.5}px ${(e.clientY - biasY)/biasY*2.5}px 30px 5px #0000001f`
-	let innerShadowParameters = `inset ${(e.clientX - biasX)/biasX*15}px ${(e.clientY - biasY)/biasY*15}px 128px 5px #0000001f`
 	referToTags('.shadow').forEach(item => {item.style['text-shadow'] = textShadowParameters})
 	referToTag('.right-side').style['box-shadow'] = shadowParameters
-	console.log(referToTag('.right-side::after'))
 }
+
+referToTag('.time').innerHTML = `${currentDate.getHours()}:${currentDate.getMinutes()}`
+referToTag('.date').innerHTML = `${currentDate.toLocaleString('default', {month: 'long'})} ${currentDate.getDay()}`
+referToTag('.day-ofz-week').innerHTML = `${currentDate.toLocaleString('default', {weekday: 'long'})}`
+referToTag('.about-day').style.width = `${referToTag('.date').clientWidth}px`
+setInterval(() => {referToTag('.about-day').style.width = `${referToTag('.date').clientWidth}px`}, 20)
+
+setInterval(() => {referToTag('.time').innerHTML = `${currentDate.getHours()}:${currentDate.getMinutes()}`}, 5000)
