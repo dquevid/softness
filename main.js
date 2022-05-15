9fe77527088f697ddcd4b123103d5485bcf6e170
DQVID <david.lord.ru@gmail.com>
2022-05-15 22:51:02 +0300

    alpha 0.4

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-let currentDate = new Date()
+let setShadow = (selector, props) => {
+	if (props.shadowType == 'textShadow') {
+		referToTag(selector).style['text-shadow'] = `${props.bias.x}px ${props.bias.y}px ${props.blur}px ${props.color}`
+		return
+	}
+
+	if (props.shadowType == 'innerShadow') {
+		referToTag(selector).style['box-shadow'] = `${referToTag(selector).style['box-shadow']}, inset ${props.bias.x}px ${props.bias.y}px ${props.blur}px ${props.size}px ${props.color}`
+		console.log(referToTag(selector).style['box-shadow'])
+		return
+	}
+
+	referToTag(selector).style['box-shadow'] = `${props.bias.x}px ${props.bias.y}px ${props.blur}px ${props.size}px ${props.color}`
+}
 
 let referToTag = (selector) => {
 	return document.querySelector(selector)
@@ -13,19 +26,44 @@ let setTime = () => {
 	referToTag('.time').innerHTML = currentDate.getHours() + ':' + currentDate.getMinutes()
 }
 
+let currentDate = new Date()
+let shadow = {
+	textShadow: {
+		elements: ['.time', '.date', '.week-day', '.weather'],
+		config: {shadowType: 'textShadow', bias: {x: 5, y: 5}, blur: 15, color: '#5544330f'}
+	},
+	boxShadow: {
+		elements: ['.right-side', '.desk-text'],
+		config: {bias: {x: 5, y: 5}, blur: 15, size: 0, color: '#5544330f'}
+	},
+	innerShadow: {
+		elements: ['.right-side', '.desk-text'],
+		config: {shadowType: 'innerShadow', bias: {x: 15, y: 15}, blur: 64, size: 25, color: '#5544330f'}
+	}
+}
+
+setShadow('.desk-text', {bias: {x: 0, y: 0}, blur: 25, size: 0, color: '#1a1c1f0f'})
+
 document.onmousemove = e => {
-	let biasX = window.innerWidth/2
-	let biasY = window.innerHeight/2
-	let textShadowParameters = `${(e.clientX - biasX)/biasX*2.5}px ${(e.clientY - biasY)/biasY*2.5}px 7.5px #0000001f`
-	let shadowParameters = `${(e.clientX - biasX)/biasX*2.5}px ${(e.clientY - biasY)/biasY*2.5}px 30px 5px #0000001f, inset ${(e.clientX - biasX)/biasX*50}px ${(e.clientY - biasY)/biasY*50}px 256px 50px #5544330f`
-	referToTags('.shadow').forEach(item => {item.style['text-shadow'] = textShadowParameters})
-	referToTag('.right-side').style['box-shadow'] = shadowParameters
+	let bias = {x: (e.clientX - window.innerWidth/2)/(window.innerWidth/2), y: (e.clientY - window.innerHeight/2)/(window.innerHeight/2)}
+	let boxShadowConfig = JSON.parse(JSON.stringify(shadow.boxShadow.config))
+	let textShadowConfig = JSON.parse(JSON.stringify(shadow.textShadow.config))
+	let innerShadowConfig = JSON.parse(JSON.stringify(shadow.innerShadow.config))
+	textShadowConfig.bias.x *= bias.x
+	textShadowConfig.bias.y *= bias.y
+	boxShadowConfig.bias.x *= bias.x
+	boxShadowConfig.bias.y *= bias.y
+	innerShadowConfig.bias.x *= bias.x
+	innerShadowConfig.bias.y *= bias.y
+	shadow.textShadow.elements.forEach(i => {setShadow(i, textShadowConfig)})
+	shadow.boxShadow.elements.forEach(i => {setShadow(i, boxShadowConfig)})
+	shadow.innerShadow.elements.forEach(i => {setShadow(i, innerShadowConfig)})
 }
 
 referToTag('.time').innerHTML = `${currentDate.getHours()}:${currentDate.getMinutes()}`
-referToTag('.date').innerHTML = `${currentDate.toLocaleString('default', {month: 'long'})} ${currentDate.getDate()}`
-referToTag('.day-ofz-week').innerHTML = `${currentDate.toLocaleString('default', {weekday: 'long'})}`
+referToTag('.date').innerHTML = `${currentDate.toLocaleString('en-US', {month: 'long'})} ${currentDate.getDate()}`
+referToTag('.week-day').innerHTML = `${currentDate.toLocaleString('en-US', {weekday: 'long'})}`
 referToTag('.about-day').style.width = `${referToTag('.date').clientWidth}px`
-setInterval(() => {referToTag('.about-day').style.width = `${referToTag('.date').clientWidth}px`}, 20)
+setInterval(() => {referToTag('.about-day').style.width = `${referToTag('.date').clientWidth}px`}, 200)
 
 setInterval(setTime, 1000)
